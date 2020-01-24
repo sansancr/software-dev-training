26abd0b46f9e4857c6579f7defa3268f60167367
SandraProgra <sandraprogra@Juans-MacBook-Pro.local>
2020-01-24 10:27:15 -0600

    lesson03

---
+++ lesson03-functions/app.js
@@ -0,0 +1,18 @@
+function printmessages(message){
+    const hello = "hola"
+    let bye = "bye"
+    bye = "adios"
+    console.log(hello)
+    console.log(bye)
+    console.log(message)
+}
+
+printmessages("hola y adios")
+
+function dia(sol,luna){
+    console.log(sol,luna)
+}
+dia("ojochal","uvita")
+
+
+
